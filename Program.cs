75cff7c42235fb9bbb558fb8fbfede61770a02e4
Felipe <felipec.almeida12@gmail.com>
2023-02-03 12:23:43 -0300

    Fiz a implementações que estavam faltando para o funcionamento do código.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+// Nokia:
+Nokia nokia = new Nokia("9123-5776", "Nokia 5.3 Dual Chip", "2222222", 32);
+Console.WriteLine("Aparelho Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+//Iphone
+Iphone iphone = new Iphone("1234-8775", "Iphone 13 Pro Max", "1111111", 128);
+Console.WriteLine("Aparelho Iphone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
